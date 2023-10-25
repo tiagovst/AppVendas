a92988a5efd09b40ad8b66a9eaa2e374d7471aa5
tiagovst <tiago.vassan@gmail.com>
2023-10-25 17:02:39 -0300

    baseline v1.0

---
+++ src/View/MainForm.dfm
@@ -0,0 +1,14 @@
+object MainView: TMainView
+  Left = 0
+  Top = 0
+  Caption = 'Nome do Produto'
+  ClientHeight = 625
+  ClientWidth = 863
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+end
---
+++ src/View/MainForm.pas
@@ -0,0 +1,31 @@
+unit MainForm;
+
+interface
+
+uses
+  Winapi.Windows,
+  Winapi.Messages,
+  System.SysUtils,
+  System.Variants,
+  System.Classes,
+  Vcl.Graphics,
+  Vcl.Controls,
+  Vcl.Forms,
+  Vcl.Dialogs;
+
+type
+  TMainView = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  MainView: TMainView;
+
+implementation
+
+{$R *.dfm}
+
+end.
