53ba65f06b0a00cb8e718a08eb059d7f52f513ec
gabriel-nunes-s <nunezdozzantos@gmail.com>
2023-12-19 17:45:26 -0300

    Telas Venda e Listagem de clientes ok

---
+++ src/View/Vendas.View.pas
@@ -0,0 +1,44 @@
+unit Vendas.View;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.pngimage,
+  Vcl.StdCtrls, Data.DB, Vcl.Grids, Vcl.DBGrids;
+
+type
+  TTelaVendas = class(TForm)
+    pnlConteudo: TPanel;
+    Panel3: TPanel;
+    Label1: TLabel;
+    Label2: TLabel;
+    Image: TImage;
+    pnlBarraTarefas: TPanel;
+    Image1: TImage;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Image2: TImage;
+    Label3: TLabel;
+    Label4: TLabel;
+    lblTotalEmVendas: TLabel;
+    Label6: TLabel;
+    lblQuantidadeItensVendidos: TLabel;
+    DBGrid1: TDBGrid;
+    Button1: TButton;
+    Button2: TButton;
+    DataSourceVendas: TDataSource;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  TelaVendas: TTelaVendas;
+
+implementation
+
+{$R *.dfm}
+
+end.
